0851fa800331157dfdcc796aba0ea23d80f3988c
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2023-12-04 15:22:40 +0100

    task 11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" function to transpose matrix
+"""
+
+
+def np_transpose(matrix):
+    """ function : transpose matrix
+
+    Arguments:
+        matrix : matrix
+
+    Returns:
+        transposed matrix
+    """
+    return matrix.transpose()
