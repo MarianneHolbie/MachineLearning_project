ac5c2050aa5f7b569e7af28f4dbc0c906ee78ccc
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2023-12-04 14:34:32 +0100

    task 5

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+""" function to add two matrix
+    use previous function to calculate shape
+"""
+
+
+def matrix_shape(matrix):
+    """ function : calculate the shape of a matrix
+
+    Arguments:
+        matrix: matrix to calculate
+
+    Returns:
+        a list of integers
+    """
+    if not matrix or not isinstance(matrix, list):
+        return []
+
+    depth = matrix_shape(matrix[0])
+
+    return [len(matrix)] + depth
+
+
+def add_matrices2D(matrix1, matrix2):
+    """ function : add two matrix
+
+    Arguments:
+        matrix1 : first matrix
+        matrix2 : second matrix
+
+    Returns:
+        new matrix
+    """
+    shape1 = matrix_shape(matrix1)
+    shape2 = matrix_shape(matrix2)
+
+    if shape1 != shape2:
+        return None
+
+    new_matrix = []
+
+    for i in range(shape1[0]):
+        row_result = []
+
+        for j in range(shape1[1]):
+            sum_element = matrix1[i][j] + matrix2[i][j]
+
+            row_result.append(sum_element)
+
+        new_matrix.append(row_result)
+
+    return new_matrix
