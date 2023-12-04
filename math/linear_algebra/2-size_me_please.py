9c39a0682129a006a8f18bf20b59cc626f707125
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2023-12-04 14:00:05 +0100

    Task 2

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def matrix_shape(matrix):
+    """ function : calculate the shape of a matrix
+
+    Arguments:
+        matrix: matrix to calculate
+
+    Returns:
+        a list of integers
+    """
+    if not matrix or not isinstance(matrix, list):
+        return []
+
+    depth = matrix_shape(matrix[0])
+
+    return [len(matrix)] + depth
