df700ce39d2f69560e49439d06cca81c775fbb25
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2023-12-04 14:59:14 +0100

    Task 7

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" function to concatenate two matrix
+"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """ function : concatenate two matrix
+
+    Arguments:
+        mat1 : first matrix
+        mat2 : second matrix
+        axis: specific axis for concatenation (0 for vertical, 1 for horizontal )
+
+    Returns:
+        new concatened matrix
+    """
+
+    if axis == 0:
+        return mat1 + mat2
+    elif axis == 1:
+        # zip :  take 2 sequences return tuples of 2 elements
+        return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
+    else:
+        return None
