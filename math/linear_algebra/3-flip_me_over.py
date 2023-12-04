c9aff43f0dbc6071aa5761ac6328dd8e607dbc06
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2023-12-04 14:25:37 +0100

    task 3

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" function to transpose a matrix"""
+
+
+def matrix_transpose(matrix):
+    """ function :transpose a matrix
+
+    Arguments:
+        matrix: matrix to calculate
+
+    Returns:
+        transposed matrix
+    """
+
+    result = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    return result
