5f9bd473d87a7db4c592d6acf6fc52a35e840741
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2023-12-04 15:20:20 +0100

    task 10

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" function to calculate the shape of a numpy.ndarray
+"""
+
+
+def np_shape(matrix):
+    """ function : calculate shape of a numpy.ndarray
+
+    Arguments:
+        matrix : matrix
+
+    Returns:
+        tuple of matrix'shape
+    """
+    return matrix.shape
