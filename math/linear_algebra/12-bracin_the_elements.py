08c1a9057563cfefc6b784a0e4cfbd2aac671a30
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2023-12-04 15:25:13 +0100

    task 12

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" function  that performs element-wise addition,
+    subtraction, multiplication, and division
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """ function : perform element-wise addition, sbstraction, multiplication, division
+
+    Arguments:
+        mat1 : first matrix
+        mat2 : second matrix
+
+    Returns:
+        tuple containing : sum, difference, product and quotient
+    """
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
