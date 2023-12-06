1b2c3ec013a30f6d21ccbff46fd13b40c38f85ae
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2023-12-06 15:57:13 +0100

    Solve pep8 + add only one parameter to slice

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -23,12 +23,16 @@ def np_slice(matrix, axes={}):
         # Create a tuple for slicing using numpy.s_
         slices = [slice(None)] * len(result_matrix.shape)
 
+        # Handle the case when only start is provided
+        if len(slice_tuple) == 1:
+            slices[axis] = slice(slice_tuple[0])
         # Handle the case when only start and stop are provided
-        if len(slice_tuple) == 2:
+        elif len(slice_tuple) == 2:
             slices[axis] = slice(slice_tuple[0], slice_tuple[1])
         # Handle the case when start, stop, and step are provided
         elif len(slice_tuple) == 3:
-            slices[axis] = slice(slice_tuple[0], slice_tuple[1], slice_tuple[2])
+            slices[axis] = slice(slice_tuple[0], slice_tuple[1],
+                                 slice_tuple[2])
 
         # Apply the slice to the corresponding axis
         result_matrix = result_matrix[tuple(slices)]
