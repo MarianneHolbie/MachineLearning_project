41a973f42c1da4a56649841021751763a8fa43ec
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2023-12-11 13:16:09 +0100

    solve pep8 + test if poly

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -7,16 +7,20 @@ def poly_derivative(poly):
 
         Arguments:
             poly: list of coefficients representing a polynomial
-                index : represent the power of x that the coefficient belongs to
+                index : represent the power of x that the coefficient
+                belongs to
 
         Returns:
-            new list of coefficients representing the derivative of the polynomial
+            new list of coefficients representing the derivative of
+            the polynomial
     """
-    if not isinstance(poly, list) or not all(isinstance(cuff, (int, float)) for cuff in poly):
+    if not poly or not isinstance(poly, list) or not all(isinstance(cuff, (int, float))
+                                                         for cuff in poly):
         return None
     else:
         # calculate derivative of polynom
-        derivative_cuff = [n * cuff for n, cuff in enumerate(poly[1:], start=1)]
+        derivative_cuff = [n * cuff for n, cuff in enumerate(poly[1:],
+                                                             start=1)]
 
         # special case derivative is 0
         if not derivative_cuff:
