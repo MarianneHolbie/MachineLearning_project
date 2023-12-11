582a7efb439f053b551da62b40bdd24934e44e2a
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2023-12-11 11:58:51 +0100

    Answer task 9

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Function that calculates sum of all i squared"""
+
+
+def summation_i_squared(n):
+    """ function : calculate sum of i squared
+
+        Arguments:
+            n: stopping condition
+
+        Returns:
+            integer value
+    """
+    # somme k^2 = n * (n+1) * (2*n+1)
+    return(n * (n + 1) * (2 * n + 1)) // 6
