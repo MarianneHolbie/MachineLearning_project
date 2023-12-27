b523d64dc3f2dfd4aae8f432ce0d0f292f589350
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2023-12-27 21:20:41 +0100

    add else before public instance attributes

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -20,12 +20,12 @@ class Neuron:
         # Manage exceptions
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
-        if nx < 1:
+        elif nx < 1:
             raise ValueError("nx must be a positive integer")
-
-        # public instance attributes
-        # W: Weights vector : initialized using random normal distribution
-        self.W = np.random.normal(loc=0, scale=0.1, size=(1, nx))
-        # b : bias & A activated output, both initialized to 0
-        self.b = 0
-        self.A = 0
+        else:
+            # public instance attributes
+            # W: Weights vector : initialized using random normal distribution
+            self.W = np.random.normal(loc=0, scale=0.1, size=(1, nx))
+            # b : bias & A activated output, both initialized to 0
+            self.b = 0
+            self.A = 0
