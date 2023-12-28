c18a634b34e94f6403d9da012aff504935934c0f
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2023-12-28 22:38:43 +0100

    add method forward_prop

---
+++ supervised_learning/classification/10-neural_network.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+"""
+    Class NeuralNetwork : NN with one hidden layer
+                          performing binary classification
+"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+        class NeuralNetwork
+    """
+
+    def __init__(self, nx, nodes):
+        """
+            class constructor
+
+            :param nx: number of input features
+            :param nodes: number of nodes in the hidden layer
+        """
+
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        # Private public instance attribute
+        # W1 & W2 normal distribution
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.normal(size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
+
+    # Getter function
+
+    @property
+    def W1(self):
+        """
+            The weights vector of hidden layer
+
+            :return: private value of W1
+        """
+        return self.__W1
+
+    @property
+    def b1(self):
+        """
+            The bias of hidden layer
+
+            :return: private value of b1
+        """
+        return self.__b1
+
+    @property
+    def A1(self):
+        """
+            The activated output of hidden layer
+
+            :return: private value of A1
+        """
+        return self.__A1
+
+    @property
+    def W2(self):
+        """
+            The weights vector of output neuron
+
+            :return: private value of W2
+        """
+        return self.__W2
+
+    @property
+    def b2(self):
+        """
+            The bias of output neuron
+
+            :return: private value of b2
+        """
+        return self.__b2
+
+    @property
+    def A2(self):
+        """
+            The activated output neuron (prediction)
+
+            :return: private value of A2
+        """
+        return self.__A2
+
+    def forward_prop(self, X):
+        """
+            method to calculate the forward propagation of NN
+
+            :param X: ndarray (shape (nx, m)) contains input data
+
+            :return: private attribute __A1 and __ A2
+        """
+        # multiplication of weight and add bias
+        Z1 = np.matmul(self.__W1, X) + self.__b1
+
+        # activation function
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+
+        Z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+
+        return self.__A1, self.__A2
