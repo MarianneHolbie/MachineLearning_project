ee9b939087146191e7de17952d5fabb26227fb5e
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-01-03 22:11:11 +0100

    change raise

--- supervised_learning/classification/26-deep_neural_network.py
+++ supervised_learning/classification/26-deep_neural_network.py
@@ -207,21 +207,20 @@ class DeepNeuralNetwork:
 
         if not isinstance(iterations, int):
             raise TypeError("iterations must be an integer")
-        if iterations < 0:
+        if iterations <=0:
             raise ValueError("iterations must be a positive integer")
         if not isinstance(alpha, float):
             raise TypeError("alpha must be a float")
-        if alpha < 0:
+        if alpha <= 0:
             raise ValueError("alpha must be positive")
         if not isinstance(verbose, bool):
             raise TypeError("verbose must be a boolean")
         if not isinstance(graph, bool):
             raise TypeError("graph must be a boolean")
-        if verbose or graph:
-            if not isinstance(step, int):
-                raise TypeError("step must be an integer")
-            if step < 0 or step > iterations:
-                raise ValueError("step must be positive and <= iterations")
+        if not isinstance(step, int):
+            raise TypeError("step must be an integer")
+        if step <= 0 or step > iterations:
+            raise ValueError("step must be positive and <= iterations")
 
         # list to store cost /iter
         costs = []
