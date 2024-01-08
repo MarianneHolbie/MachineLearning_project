0eb36204f4b7a5f91af832e823eb46a79f297592
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-01-08 14:28:45 +0100

    add method calculate loss

---
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+    Function softmax cross-entropy loss
+"""
+
+import tensorflow.compat.v1 as tf
+
+
+def calculate_loss(y, y_pred):
+    """
+        Method to calculate the softmax cross-entropy loss
+        of a preidction
+
+        :param y: placeholder for labels input data
+        :param y_pred: tensor network's prediction
+
+        :return: tensor loss prediction
+    """
+
+    loss = tf.compat.v1.losses.softmax_cross_entropy(
+        onehot_labels=y,
+        logits=y_pred)
+
+    return loss
