8f218fb80be58344d0e4a5fc8a995f2fa489d6c9
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-01-15 22:14:42 +0100

    method calculate moving average

--- supervised_learning/tensorflow/4-calculate_loss.py
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -9,7 +9,7 @@ import tensorflow.compat.v1 as tf
 def calculate_loss(y, y_pred):
     """
         Method to calculate the softmax cross-entropy loss
-        of a preidction
+        of a prediction
 
         :param y: placeholder for labels input data
         :param y_pred: tensor network's prediction
