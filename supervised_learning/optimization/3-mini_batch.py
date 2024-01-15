fe3b70839961a3beb703468478b717a7dfdf1aa1
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-01-15 20:38:12 +0100

    add train_op in step loop

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -7,17 +7,20 @@ import tensorflow.compat.v1 as tf
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
-def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32, epochs=5, load_path="/tmp/model.ckpt",
+def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
+                     epochs=5, load_path="/tmp/model.ckpt",
                      save_path="/tmp/model.ckpt"):
     """
-        Function trains a loaded neural network model using mini-batch gradient descent
+        Function trains a loaded neural network model using
+        mini-batch gradient descent
 
         :param X_train: ndarray, shape(m,784), training data
         :param Y_train: ndarray, shape(m,10), training labels
         :param X_valid: ndarray, shape(m,784), validation data
         :param Y_valid: ndarray, shape(m,10), validation labels
         :param batch_size: number of data points in batch
-        :param epochs: number of times the training should pass through the whole dataset
+        :param epochs: number of times the training should pass
+               through the whole dataset
         :param load_path: path from which to load the model
         :param save_path: path to where save model after training
 
@@ -26,7 +29,7 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32, epochs=5
     # metagraphe and restore session
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        new_saver = tf.train.import_meta_graph('graph.ckpt.meta')
+        new_saver = tf.train.import_meta_graph(load_path + ".meta")
         new_saver.restore(sess, load_path)
 
         # get following tensors and op from collection restored
@@ -57,11 +60,13 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32, epochs=5
 
             len_dataset = len(train_dataset[0])
 
-            for step_number in range(len_dataset//32):
+            for step_number in range(len_dataset//batch_size):
                 firs_index = step_number * batch_size
                 last_index = (step_number + 1) * batch_size
                 x_batch = train_dataset[0][firs_index: last_index]
                 y_batch = train_dataset[1][firs_index: last_index]
+
+                sess.run(train_op, feed_dict={x: x_batch, y: y_batch})
 
                 step_cost, step_accuracy = sess.run(
                     [loss, accuracy], feed_dict={x: x_batch, y: y_batch})
