55df40967c013965d4ef39c045d1f06d7f137c46
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-01-15 11:45:08 +0100

    not permutation on matrices but on index !

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -5,6 +5,7 @@
 
 import numpy as np
 
+
 def shuffle_data(X, Y):
     """
         Function that shuffles the data points in two matrices the same way
@@ -12,8 +13,10 @@ def shuffle_data(X, Y):
         :param X: ndarray, shape(m, nx) to shuffle
         :param Y: ndarray, shape(m, ny) to shuffle
 
-        :return: shuffled X and Y atrices
+        :return: shuffled X and Y matrices
     """
-    X_shuffled = np.random.permutation(X)
-    Y_shuffled = np.random.permutation(Y)
+    m = X.shape[0]
+    permutted_index = np.random.permutation(m)
+    X_shuffled = X[permutted_index]
+    Y_shuffled = Y[permutted_index]
     return X_shuffled, Y_shuffled
