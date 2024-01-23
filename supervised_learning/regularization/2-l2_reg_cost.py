73cebaae2be38c774a93c732713e167c58959348
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-01-23 09:15:25 +0100

    initialize cost L2 regularization

---
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+    Cost of NN with L2 regularization
+"""
+
+import numpy as np
+import tensorflow.compat.v1 as tf
+
+
+def l2_reg_cost(cost):
+    """
+        Function that calculate the cost of NN with L2 regularization
+
+        :param cost: tensor containing cost without L2 regularization
+
+        :return: tensor cost WITH L2 regularization
+    """
+    regularization_L2 = tf.compat.v1.losses.get_regularization_loss()
+
+    cost_L2reg = cost + regularization_L2
+
+    return cost_L2reg
