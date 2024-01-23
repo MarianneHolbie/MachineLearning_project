0714630b67be500775b6944cd3eb12513817a783
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-01-23 10:27:16 +0100

    initialize create layer with L2 regularization

---
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+    Create layer with L2 regularization
+"""
+
+import tensorflow.compat.v1 as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """
+        Function that creates a tensorflow layer includes L2 regularization
+
+        :param prev: tensor, output of previous layer
+        :param n: number of nodes the new layer
+        :param activation: activation function used on the layer
+        :param lambtha: L2 regularization parameter
+
+        :return: output of the new layer
+    """
+    # set initialization to He et. al
+    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+
+    # create layer Dense with parameters
+    new_layer = tf.layers.Dense(n,
+                                activation=activation,
+                                kernel_initializer=initializer,
+                                kernel_regularizer=tf.keras.regularizers.l2(l=lambtha),
+                                name="layer")
+
+    # apply layer to input
+    output = new_layer(prev)
+
+    return output
