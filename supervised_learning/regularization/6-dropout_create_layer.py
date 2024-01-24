59a4529436fb81c34cc79db94823fa2c7f5f445d
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-01-24 14:45:24 +0100

    add scale in initializer

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -21,11 +21,18 @@ def dropout_create_layer(prev, n, activation, keep_prob):
     # define layer Dropout
     dropout_layer = tf.compat.v1.layers.Dropout(rate=keep_prob)
 
+    # set initialization to He et. al
+    initializer = tf.keras.initializers.VarianceScaling(scale=2.0,
+                                                        mode='fan_avg')
+
     # apply dropout
     new_layer = (
         tf.layers.Dense(n,
                         activation=activation,
+                        kernel_initializer=initializer,
+                        kernel_regularizer=dropout_layer,
                         name="layer"))
-    output = dropout_layer(new_layer(prev))
+
+    output = new_layer(prev)
 
     return output
