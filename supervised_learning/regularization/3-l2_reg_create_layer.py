8f9c7f9c3983bd7fb7688e8e6b30ce74db5138b0
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-01-24 14:40:53 +0100

    solve pycode

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -18,7 +18,8 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
         :return: output of the new layer
     """
     # set initialization to He et. al
-    initializer = tf.keras.initializers.VarianceScaling(scale=2.0, mode='fan_avg')
+    initializer = tf.keras.initializers.VarianceScaling(scale=2.0,
+                                                        mode='fan_avg')
 
     # create layer Dense with parameters
     new_layer = (
