8d18d30c144068e7247bf4494cca580c26535477
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-02-01 10:25:43 +0100

    add forgotten stride in convolution and generalize calculation of output heigth and width

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -9,7 +9,6 @@ import numpy as np
 def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     """
         Function that performs a convolution on grayscale images
-        with custom padding
 
         :param images: ndarray, shape(m, h, w), multiple grayscale images
         :param kernel: ndarray, shape(kh,kw), kernel for convolution
@@ -21,40 +20,46 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     # size images, kernel, padding, stride
     m, h, w = images.shape
     kh, kw = kernel.shape
-    if isinstance(padding, tuple):
-        ph, pw = padding
-    else:
-        ph, pw = None, None
     sh, sw = stride
 
     # output size and padding
-    if ph is not None and pw is not None:
-        output_height = int((h - kh + 2 * ph) / sh + 1)
-        output_width = int((w - kw + 2 * pw) / sw + 1)
-        image_pad = np.pad(images,
-                           ((0, 0), (ph, ph),
-                            (pw, pw)), mode='constant')
-    elif padding == 'valid':
-        output_height = int((h - kh + 1) / sh)
-        output_width = int((w - kw + 1) / sw)
-        image_pad = images
-    else:
-        output_height = int(h / sh)
-        output_width = int(w / sw)
-        padding_width = int(kw / 2)
-        padding_height = int(kh / 2)
-        image_pad = np.pad(images,
-                           ((0, 0), (padding_height, padding_height),
-                            (padding_width, padding_width)), mode='constant')
+    if padding == 'valid':
+        # no padding
+        ph, pw = 0, 0
+    elif padding == 'same':
+        """
+            (h - 1) * sh : total distance traveled by the filter
+             when moving vertically over the image
+            (+ kh - h): size of the filter (kh) subtract the height image (h)
+             to ensure the filter stays entirely within the image.
+            /2 : get the distance from the center of the image 
+            to the top or bottom edge
+            + 1 : add 1 to ensure that even if the distance is a decimal, 
+            we round up to ensure the filter stays entirely within the image.
+            """
+        ph = int((((h - 1) * sh + kh - h)/2) + 1)
+        pw = int((((w - 1) * sw + kw - w)/2) + 1)
+
+    elif isinstance(padding, tuple):
+        ph, pw = padding
+
+    # generalize output calcul
+    output_height = int((h - kh + 2 * ph) / sh + 1)
+    output_width = int((w - kw + 2 * pw) / sw + 1)
 
     # initialize output
     convolved_images = np.zeros((m, output_height, output_width))
+
+    # pad image
+    image_pad = np.pad(images,
+                       ((0, 0), (ph, ph),
+                        (pw, pw)), mode='constant')
 
     # convolution
     for i in range(output_height):
         for j in range(output_width):
             # extract region from each image
-            image_zone = image_pad[:, i:i + kh, j:j + kw]
+            image_zone = image_pad[:, i * sh:i * sh + kh, j * sw:j * sw + kw]
 
             # element wize multiplication
             convolved_images[:, i, j] = np.sum(image_zone * kernel,
