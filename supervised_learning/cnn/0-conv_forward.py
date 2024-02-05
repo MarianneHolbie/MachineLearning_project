d2d79f7ca2df713409ee2d4456aa7ff9b24536e6
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-02-05 11:03:21 +0100

    solve case of same padding, remove + 1

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -29,8 +29,8 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
         # no padding
         ph, pw = 0, 0
     elif padding == 'same':
-        ph = int((((h_prev - 1) * sh + kh - h_prev) / 2) + 1)
-        pw = int((((w_prev - 1) * sw + kw - w_prev) / 2) + 1)
+        ph = int((((h_prev - 1) * sh + kh - h_prev) / 2))
+        pw = int((((w_prev - 1) * sw + kw - w_prev) / 2))
 
     # output size
     output_height = int((h_prev - kh + 2 * ph) / sh + 1)
