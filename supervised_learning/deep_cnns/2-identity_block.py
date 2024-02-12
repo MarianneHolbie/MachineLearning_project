90275a131a893169de8b4827972fd5f8edd1cee1
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-02-12 14:04:12 +0100

    user   layers.Activation instead of layers.ReLU

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -42,7 +42,7 @@ def identity_block(A_prev, filters):
                             padding='same')(relu1)
 
     batchN2 = K.layers.BatchNormalization(axis=3)(conv2)
-    relu2 = K.layers.ReLU()(batchN2)
+    relu2 = K.layers.Activation(activation='relu')(batchN2)
 
     # third layer
     conv3 = K.layers.Conv2D(F12,
@@ -51,7 +51,7 @@ def identity_block(A_prev, filters):
                             strides=(1, 1),
                             padding='same')(relu2)
     batchN3 = K.layers.BatchNormalization(axis=3)(conv3)
-    relu3 = K.layers.ReLU()(batchN3)
+    relu3 = K.layers.Activation(activation='relu')(batchN3)
 
     # add input (Residual Network)
     resnet = K.layers.add([relu3, A_prev])
