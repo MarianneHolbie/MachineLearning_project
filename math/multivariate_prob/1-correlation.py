0a4e3e22886ff165811bbee0e75bfcdafff593f6
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-03-18 16:30:44 +0100

    add condition to test dimension of C

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -15,7 +15,7 @@ def correlation(C):
     """
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-    if C.shape[0] != C.shape[1]:
+    if C.ndim != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
 
     # calcul of standard deviation
