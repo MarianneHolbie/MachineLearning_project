648b5809c7af06fc211fabc3c2931d4c4062849e
Marianne Arrué <112870394+MarianneHolbie@users.noreply.github.com>
2024-03-18 16:25:15 +0100

    function to calculate correlation matrix

---
+++ math/multivariate_prob/1-correlation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+    Correlation
+"""
+import numpy as np
+
+
+def correlation(C):
+    """
+        function that calculates a correlation matrix
+
+        :param C: ndarray, shape(d,d) covariance matrix
+                d: number of dimensions
+        :return: ndarray, shape(d,d), correlation matrix
+    """
+    if not isinstance(C, np.ndarray):
+        raise TypeError("C must be a numpy.ndarray")
+    if C.shape[0] != C.shape[1]:
+        raise ValueError("C must be a 2D square matrix")
+
+    # calcul of standard deviation
+    std_dev = np.sqrt(np.diag(C))
+
+    # correlation matrix
+    d = C.shape[0]
+    corr = np.zeros((d, d))
+
+    for i in range(d):
+        for j in range(d):
+            corr[i, j] = C[i, j] / (std_dev[i] * std_dev[j])
+
+    return corr
